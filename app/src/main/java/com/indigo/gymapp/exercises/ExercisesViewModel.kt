29f185d3640043cec48f915666d46c852f2e9bd3
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-04 15:36:05 -0300

    Exercises screen

---
+++ app/src/main/java/com/indigo/gymapp/exercises/ExercisesViewModel.kt
@@ -0,0 +1,55 @@
+package com.indigo.gymapp.exercises
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.indigo.gymapp.domain.exercises.Exercise
+import com.indigo.gymapp.domain.exercises.ExercisesServiceApi
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExercisesViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val exercisesServiceApi: ExercisesServiceApi,
+): ViewModel() {
+    private val _loadingExercises = MutableStateFlow(false)
+    val loadingExercises = _loadingExercises.asStateFlow()
+
+    private val _exercises = MutableStateFlow(listOf<Exercise>())
+    val exercises = _exercises.asStateFlow()
+
+    private val _showRetry = MutableStateFlow(false)
+    val showRetry = _showRetry.asStateFlow()
+
+    init {
+        loadRanking()
+    }
+
+    fun retryLoadingRanking() {
+        loadRanking()
+    }
+
+    private fun loadRanking() {
+        _loadingExercises.value = true
+        exercisesServiceApi.getExercises(
+            context = context,
+            onSuccess = {
+                viewModelScope.launch {
+                    _exercises.value = it
+                }
+                _showRetry.value = false
+            },
+            onFail = {
+                _showRetry.value = true
+            },
+            loadingFinished = {
+                _loadingExercises.value = false
+            }
+        )
+    }
+}
