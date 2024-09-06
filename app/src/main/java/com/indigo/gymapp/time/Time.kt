4d0ab15b96ca4c56a4557bc40d0d7e63e2cc9b4a
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-06 11:55:19 -0300

    cleaned up displaySeconds

--- app/src/main/java/com/indigo/gymapp/time/Time.kt
+++ app/src/main/java/com/indigo/gymapp/time/Time.kt
@@ -2,19 +2,7 @@ package com.indigo.gymapp.time
 
 import androidx.annotation.IntRange
 
-fun displaySeconds(seconds: Int) = when (seconds) {
-    0 -> "00"
-    1 -> "01"
-    2 -> "02"
-    3 -> "03"
-    4 -> "04"
-    5 -> "05"
-    6 -> "06"
-    7 -> "07"
-    8 -> "08"
-    9 -> "09"
-    else -> "$seconds"
-}
+fun displaySeconds(seconds: Int) = if (seconds in 0 .. 9) "0$seconds" else seconds
 
 data class Time(
     @IntRange(from = 0, to = 59)
