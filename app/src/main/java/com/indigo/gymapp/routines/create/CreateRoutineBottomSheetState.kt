4d6a2a0e3981c3e065b466a9b407724d74be37e4
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-06 12:04:11 -0300

    CreateRoutineBottomSheetState

---
+++ app/src/main/java/com/indigo/gymapp/routines/create/CreateRoutineBottomSheetState.kt
@@ -0,0 +1,13 @@
+package com.indigo.gymapp.routines.create
+
+sealed interface CreateRoutineBottomSheetState {
+    fun showBottomSheet() : Boolean
+}
+data object Closed : CreateRoutineBottomSheetState {
+    override fun showBottomSheet(): Boolean = false
+}
+sealed interface Open : CreateRoutineBottomSheetState {
+    override fun showBottomSheet(): Boolean = true
+}
+
+data object NameYourRoutine : Open
