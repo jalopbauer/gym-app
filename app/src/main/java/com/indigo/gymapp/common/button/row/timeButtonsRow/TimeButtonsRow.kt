0eba13bad6842221354d1d4c408e6f88a42dd3db
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-06 13:12:15 -0300

    TimeButtonsRow

---
+++ app/src/main/java/com/indigo/gymapp/common/button/row/timeButtonsRow/TimeButtonsRow.kt
@@ -0,0 +1,40 @@
+package com.indigo.gymapp.common.button.row.timeButtonsRow
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.indigo.gymapp.common.button.timeButton.TimeButton
+import com.indigo.gymapp.time.Time
+
+@Composable
+fun TimeButtonsRow(
+    leftTime: Time,
+    leftTimeOnClick: (Time) -> Unit,
+    centerTime: Time,
+    centerTimeOnClick: (Time) -> Unit,
+    rightTime: Time,
+    rightTimeOnClick: (Time) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterHorizontally),
+    ) {
+        TimeButton(
+            time = leftTime,
+            onClick = leftTimeOnClick
+        )
+        TimeButton(
+            time = centerTime,
+            onClick = centerTimeOnClick
+        )
+        TimeButton(
+            time = rightTime,
+            onClick = rightTimeOnClick
+        )
+    }
+}
