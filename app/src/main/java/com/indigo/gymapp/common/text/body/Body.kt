0d34f6de83ca6471f5840b4991e61cee0c88e43f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-06 12:11:17 -0300

    added TextSize to Body

--- app/src/main/java/com/indigo/gymapp/common/text/body/Body.kt
+++ app/src/main/java/com/indigo/gymapp/common/text/body/Body.kt
@@ -3,6 +3,8 @@ package com.indigo.gymapp.common.text.body
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.indigo.gymapp.common.text.Large
+import com.indigo.gymapp.common.text.Medium
 import com.indigo.gymapp.common.text.Small
 import com.indigo.gymapp.common.text.TextSize
 import com.indigo.gymapp.ui.theme.color.Color
@@ -12,6 +14,10 @@ fun Body(body: String, textSize : TextSize = Small) {
     Text(
         text = body,
         color = Color.Context.Text.primary,
-        style = MaterialTheme.typography.bodySmall
+        style = when(textSize) {
+                    Large -> MaterialTheme.typography.bodyLarge
+                    Medium -> MaterialTheme.typography.bodyMedium
+                    Small -> MaterialTheme.typography.bodySmall
+                }
     )
 }
