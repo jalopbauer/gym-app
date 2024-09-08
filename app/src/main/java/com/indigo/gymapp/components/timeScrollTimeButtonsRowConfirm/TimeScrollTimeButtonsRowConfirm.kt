57723ab74d8c73d2c42691e2bc260024b5de2929
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-08 15:27:26 -0300

    TimeScrollTimeButtonsRowConfirm

--- app/src/main/java/com/indigo/gymapp/components/timeScrollTimeButtonsRowConfirm/TimeScrollTimeButtonsRowConfirm.kt
+++ app/src/main/java/com/indigo/gymapp/components/timeScrollTimeButtonsRowConfirm/TimeScrollTimeButtonsRowConfirm.kt
@@ -1,16 +1,19 @@
-package com.indigo.gymapp.components.timeScrollTimeButtonsRow
+package com.indigo.gymapp.components.timeScrollTimeButtonsRowConfirm
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.indigo.gymapp.R
+import com.indigo.gymapp.common.button.Button
 import com.indigo.gymapp.common.button.row.timeButtonsRow.TimeButtonsRow
 import com.indigo.gymapp.components.scroll.timeScroll.TimeScroll
 import com.indigo.gymapp.time.Rest
 
 
 @Composable
-fun TimeScrollTimeButtonsRow(
+fun TimeScrollTimeButtonsRowConfirm(
     routineRestTimeBetweenExercises: Rest,
     selectedMinutes: (Int) -> Unit,
     selectedSeconds: (Int) -> Unit,
@@ -20,6 +23,7 @@ fun TimeScrollTimeButtonsRow(
     centerTimeOnClick: (Rest) -> Unit,
     rightTime: Rest,
     rightTimeOnClick: (Rest) -> Unit,
+    confirmOnClick: () -> Unit,
 ) {
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -32,6 +36,10 @@ fun TimeScrollTimeButtonsRow(
             centerTimeOnClick,
             rightTime,
             rightTimeOnClick
+        )
+        Button(
+            text = stringResource(R.string.confirm),
+            onClick = confirmOnClick
         )
     }
 }
