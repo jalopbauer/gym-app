c07b22d7992dbf7ebd882ce8de7804c028fa825f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-18 16:45:22 -0300

    Removed unused text

--- app/src/main/java/com/indigo/gymapp/pages/configuration/Configuration.kt
+++ app/src/main/java/com/indigo/gymapp/pages/configuration/Configuration.kt
@@ -1,9 +1,7 @@
 package com.indigo.gymapp.pages.configuration
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Configuration() {
-    Text(text = "Config")
 }
