f7fc2e459c55d0713bddca12ea938d8d3b9f3062
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-18 15:24:09 -0300

    LocalExercises page

---
+++ app/src/main/java/com/indigo/gymapp/pages/exercises/LocalExercises.kt
@@ -0,0 +1,43 @@
+package com.indigo.gymapp.pages.exercises
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.indigo.gymapp.common.button.Button
+import com.indigo.gymapp.common.text.headline.Headline
+import com.indigo.gymapp.common.textField.TextField
+import com.indigo.gymapp.ui.spacing.Spacing.Context
+
+@Composable
+fun LocalExercises() {
+    val exerciseViewModel = hiltViewModel<ExerciseViewModel>()
+    val exercises by exerciseViewModel.exercises.collectAsState(initial = emptyList())
+    var newExerciseName by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(Context.Gap.default)
+    ) {
+        Column {
+            TextField(
+                value = newExerciseName,
+                label = "ExerciseName",
+                onValueChange = { newExerciseName = it }
+            )
+            Button(
+                text = "Add exercise",
+                onClick = { exerciseViewModel.createExercise(newExerciseName) }
+            )
+        }
+        exercises.forEach { exercise ->
+            Headline(exercise.name)
+        }
+    }
+}
