ce200e080d33d168344b7e2ab59f091cce4ded0a
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-18 16:45:06 -0300

    Removed unused text

--- app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
+++ app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
@@ -1,9 +1,7 @@
 package com.indigo.gymapp.pages.calendar
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Calendar() {
-    Text(text = "Calendar")
 }
