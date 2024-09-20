0c8bc7f9f3efc1f31fb187ba2edcb2aeaebc6155
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-20 11:04:02 -0300

    Vertical number scroll

---
+++ app/src/main/java/com/indigo/gymapp/common/numberScroll/VerticalNumberScroll.kt
@@ -0,0 +1,66 @@
+package com.indigo.gymapp.common.numberScroll
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.indigo.gymapp.common.text.Large
+import com.indigo.gymapp.common.text.label.Label
+import com.indigo.gymapp.ui.spacing.Spacing
+import com.indigo.gymapp.ui.theme.color.Color.Context
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun VerticalNumberScroll(
+    label: String,
+    maxValue: Int,
+    startingIndex: Int = 0,
+    indexDisplay: (Int) -> String = { "$it" },
+    selectedItem: (Int) -> Unit
+) {
+    val pagerState = rememberPagerState(
+        initialPage = startingIndex,
+        pageCount = { maxValue }
+    )
+    selectedItem(pagerState.settledPage)
+    val verticalPagerHeight = 360.dp
+    val pageSize = 64.dp
+    Column (
+        verticalArrangement = Arrangement.spacedBy(Spacing.Context.Gap.default),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Label(
+            label = label,
+            textSize = Large
+        )
+        VerticalPager(
+            state = pagerState,
+            pageSize = PageSize.Fixed(pageSize),
+            modifier = Modifier.height(verticalPagerHeight),
+            horizontalAlignment = Alignment.End,
+            contentPadding = PaddingValues(vertical = (verticalPagerHeight - pageSize) / 2)
+
+        ) { page ->
+            val isSelected = page == pagerState.currentPage
+            Text(
+                modifier = Modifier.width(pageSize),
+                text = indexDisplay(page),
+                style = MaterialTheme.typography.displayLarge,
+                color = if (isSelected) Context.Text.primary else Context.Text.information,
+                textAlign = TextAlign.Right
+            )
+        }
+    }
+}
--- app/src/main/java/com/indigo/gymapp/components/scroll/timeScroll/TimeScroll.kt
+++ app/src/main/java/com/indigo/gymapp/components/scroll/timeScroll/TimeScroll.kt
@@ -7,8 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.indigo.gymapp.R
-import com.indigo.gymapp.common.numberScroll.MinuteAndSecondsVerticalScroll
+import com.indigo.gymapp.common.numberScroll.VerticalNumberScroll
 import com.indigo.gymapp.domain.time.Time
 import com.indigo.gymapp.domain.time.displaySeconds
 
@@ -20,18 +21,20 @@ fun TimeScroll(
 ) {
     Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.spacedBy(space = 32.dp, alignment = Alignment.CenterHorizontally),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        MinuteAndSecondsVerticalScroll(
+        VerticalNumberScroll(
             label = stringResource(R.string.minutes),
             startingIndex = startingTime.minutes,
+            maxValue = 59,
             selectedItem = selectedMinutes
         )
-        MinuteAndSecondsVerticalScroll(
+        VerticalNumberScroll(
             label = stringResource(R.string.seconds),
             startingIndex = startingTime.seconds,
             selectedItem = selectedSeconds,
+            maxValue = 59,
             indexDisplay = { displaySeconds(it) }
         )
     }
