6a829a2c3376c35b7d0db4b4b44ed567a2a58a97
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-25 18:33:22 -0300

    DataStore file

---
+++ app/src/main/java/com/indigo/gymapp/data/store/DataStore.kt
@@ -0,0 +1,23 @@
+package com.indigo.gymapp.data.store
+
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "UNSCRAMBLE_DATA_STORE")
+object PreferencesKeys {
+}
+suspend fun <T> saveToDataStore(context: Context, value: T, key: Preferences.Key<T>) {
+    context.dataStore.edit { preferences ->
+        preferences[key] = value
+    }
+}
+fun <T> getFromDataStore(context: Context, key: Preferences.Key<T>): Flow<T?> {
+    return context.dataStore.data
+        .map { preferences ->
+            preferences[key]
+        }
+}
