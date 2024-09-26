76e78d7bbe3007140092f31dc5ad6e8197a9e215
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-26 15:43:46 -0300

    ExerciseTextButton

---
+++ app/src/main/java/com/indigo/gymapp/pages/routines/create/exercise/create/exerciseTextButton/ExerciseTextButton.kt
@@ -0,0 +1,30 @@
+package com.indigo.gymapp.pages.routines.create.exercise.create.exerciseTextButton
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.indigo.gymapp.common.text.headline.Headline
+import com.indigo.gymapp.domain.exercises.Exercise
+
+@Composable
+fun ExerciseTextButton(
+    exercise: Exercise,
+    getExerciseOnClick: (Exercise) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .height(48.dp)
+            .fillMaxWidth()
+            .clickable {
+                getExerciseOnClick(exercise)
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Headline(exercise.name)
+    }
+}
