2975bac846aac15eefb77fb928bd34815556a058
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-09-26 14:22:13 -0300

    fix on reload wrong app bar

--- app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
+++ app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
@@ -1,7 +1,15 @@
 package com.indigo.gymapp.pages.calendar
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.indigo.gymapp.manager.bottomAppBar.BottomAppBarViewModel
 
 @Composable
 fun Calendar() {
+    val bottomAppBarViewModel = hiltViewModel<BottomAppBarViewModel>()
+
+    LaunchedEffect(Unit) {
+        bottomAppBarViewModel.setNavigation()
+    }
 }
