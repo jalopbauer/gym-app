bb70a82ceb322e277dfc8b391ebb2004d0e77012
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-10-06 14:45:10 -0300

    Handled TextField lose focus whenever you click anywhere & on add exercise

--- app/src/main/java/com/indigo/gymapp/exercises/local/LocalExercises.kt
+++ app/src/main/java/com/indigo/gymapp/exercises/local/LocalExercises.kt
@@ -1,8 +1,10 @@
 package com.indigo.gymapp.exercises.local
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -11,8 +13,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.indigo.gymapp.R
@@ -25,7 +28,7 @@ import com.indigo.gymapp.ui.spacing.Spacing.Context
 @Composable
 fun LocalExercises() {
     val context = LocalContext.current
-    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
 
     val exerciseViewModel = hiltViewModel<ExerciseViewModel>()
     val exercises by exerciseViewModel.exercises.collectAsState(initial = emptyList())
@@ -34,6 +37,9 @@ fun LocalExercises() {
     }
 
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable { focusManager.clearFocus() },
         verticalArrangement = Arrangement.spacedBy(Context.Gap.default)
     ) {
         TextField(
@@ -47,7 +53,7 @@ fun LocalExercises() {
                 exerciseViewModel.createExercise(newExerciseName)
                 Toast.makeText(context, context.getString(R.string.exercise_added, newExerciseName), Toast.LENGTH_SHORT).show()
                 newExerciseName = ""
-                keyboardController?.hide()
+                focusManager.clearFocus()
             }
         )
         LazyColumn (
