6ffcff0564623113c1a7c713f3bba7f70417e51c
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-10-16 15:03:55 -0300

    Made MainActivity FragmentActivity instead of ComponentActivity for BiometricAuth

--- app/src/main/java/com/indigo/gymapp/MainActivity.kt
+++ app/src/main/java/com/indigo/gymapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.indigo.gymapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.compose.rememberNavController
 import com.indigo.gymapp.common.bottomAppBar.BottomAppBar
 import com.indigo.gymapp.common.bottomAppBar.CreateUpdateDeleteBottomAppBar
@@ -24,7 +24,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
 
     @Inject
     lateinit var bottomAppBarManagerSingleton : BottomAppBarManagerSingleton
