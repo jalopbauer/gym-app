15242ae083dca07b342f3db5c5b9b56591d35e77
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-10-23 14:50:36 -0300

    Added headline sizes

--- app/src/main/java/com/indigo/gymapp/common/text/headline/Headline.kt
+++ app/src/main/java/com/indigo/gymapp/common/text/headline/Headline.kt
@@ -3,6 +3,8 @@ package com.indigo.gymapp.common.text.headline
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.indigo.gymapp.common.text.Large
+import com.indigo.gymapp.common.text.Medium
 import com.indigo.gymapp.common.text.Small
 import com.indigo.gymapp.common.text.TextSize
 import com.indigo.gymapp.ui.theme.color.Color
@@ -12,6 +14,10 @@ fun Headline(headline: String, textSize : TextSize = Small) {
     Text(
         text = headline,
         color = Color.Context.Text.primary,
-        style = MaterialTheme.typography.headlineSmall
+        style = when(textSize) {
+            Large -> MaterialTheme.typography.headlineLarge
+            Medium -> MaterialTheme.typography.headlineMedium
+            Small -> MaterialTheme.typography.headlineSmall
+        }
     )
 }
