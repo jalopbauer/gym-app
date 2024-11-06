c23f7be147d6a1154477697cce093393f001039a
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-06 16:56:12 -0300

    Configuration Headline

--- app/src/main/java/com/indigo/gymapp/pages/configuration/Configuration.kt
+++ app/src/main/java/com/indigo/gymapp/pages/configuration/Configuration.kt
@@ -1,7 +1,27 @@
 package com.indigo.gymapp.pages.configuration
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.indigo.gymapp.R
+import com.indigo.gymapp.common.text.Large
+import com.indigo.gymapp.common.text.headline.Headline
+import com.indigo.gymapp.ui.spacing.Spacing.Context.Padding
 
 @Composable
 fun Configuration() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = Padding.screen),
+    ) {
+        Headline(
+            headline = stringResource(R.string.configuration),
+            textSize = Large
+        )
+    }
 }
