1d0a2fa05522e5a75da2d11fb33713187bdd06a2
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-06 17:21:54 -0300

    Added color to icon & added icons Calendar, Routines & Configuration

--- app/src/main/java/com/indigo/gymapp/common/icon/Icon.kt
+++ app/src/main/java/com/indigo/gymapp/common/icon/Icon.kt
@@ -1,16 +1,20 @@
 package com.indigo.gymapp.common.icon
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.List
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.CalendarToday
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Replay
 import androidx.compose.material.icons.outlined.Save
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.Timer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
@@ -18,17 +22,20 @@ import androidx.compose.ui.res.stringResource
 import com.indigo.gymapp.R
 
 @Composable
-fun Icon(iconVariant : IconVariant) {
+fun Icon(iconVariant : IconVariant, tint: Color = MaterialTheme.colorScheme.primary) {
     when (iconVariant) {
-        Save -> SaveIcon()
-        Cancel -> CancelIcon()
-        Delete -> DeleteIcon()
-        Edit -> EditIcon()
-        Add -> AddIcon()
-        SetRoutineExercise -> SetRoutineExerciseIcon()
-        TimedRoutineExercise -> TimedRoutineExerciseIcon()
-        Search -> SearchIcon()
-        Exercise -> ExerciseIcon()
+        Save -> SaveIcon(tint)
+        Cancel -> CancelIcon(tint)
+        Delete -> DeleteIcon(tint)
+        Edit -> EditIcon(tint)
+        Add -> AddIcon(tint)
+        SetRoutineExercise -> SetRoutineExerciseIcon(tint)
+        TimedRoutineExercise -> TimedRoutineExerciseIcon(tint)
+        Search -> SearchIcon(tint)
+        Exercise -> ExerciseIcon(tint)
+        Calendar -> CalendarIcon(tint)
+        Routines -> RoutinesIcon(tint)
+        Configuration -> ConfigurationIcon(tint)
     }
 }
 
@@ -36,95 +43,133 @@ fun Icon(iconVariant : IconVariant) {
 private fun ImageVectorIcon(
     imageVector: ImageVector,
     contentDescription: String,
+    tint: Color = MaterialTheme.colorScheme.primary,
 ) {
     androidx.compose.material3.Icon(
         imageVector = imageVector,
         contentDescription = contentDescription,
-        tint = MaterialTheme.colorScheme.primary
+        tint = tint
     )
 }
 
 @Composable
 private fun PainterIcon(
     painter: Painter,
-    contentDescription: String
+    contentDescription: String,
+    tint: Color = MaterialTheme.colorScheme.primary,
 ) {
     androidx.compose.material3.Icon(
         painter = painter,
         contentDescription = contentDescription,
-        tint = MaterialTheme.colorScheme.primary
+        tint = tint
     )
 }
 
 @Composable
-fun CancelIcon() {
+fun CancelIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Close,
-        contentDescription = stringResource(R.string.cancel)
+        contentDescription = stringResource(R.string.cancel),
+        tint = tint
     )
 }
 
 @Composable
-fun SaveIcon() {
+fun SaveIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Save,
         contentDescription = stringResource(R.string.save),
+        tint = tint
     )
 }
 
 @Composable
-fun DeleteIcon() {
+fun DeleteIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Delete,
         contentDescription = stringResource(R.string.delete),
+        tint = tint
     )
 }
 
 @Composable
-fun EditIcon() {
+fun EditIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Edit,
         contentDescription = stringResource(R.string.edit),
+        tint = tint
     )
 }
 
 @Composable
-fun AddIcon() {
+fun AddIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Add,
         contentDescription = stringResource(R.string.add),
+        tint = tint
     )
 }
 
 @Composable
-fun SetRoutineExerciseIcon() {
+fun SetRoutineExerciseIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Replay,
         contentDescription = stringResource(R.string.set),
+        tint = tint
     )
 }
 
 @Composable
-fun TimedRoutineExerciseIcon() {
+fun TimedRoutineExerciseIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Timer,
         contentDescription = stringResource(R.string.timed),
+        tint = tint
     )
 }
 
 @Composable
-fun SearchIcon() {
+fun SearchIcon(tint: Color) {
     ImageVectorIcon(
         imageVector = Icons.Outlined.Search,
         contentDescription = stringResource(R.string.search),
+        tint = tint
     )
 }
 
 @Composable
-fun ExerciseIcon() {
+fun CalendarIcon(tint: Color) {
+    ImageVectorIcon(
+        imageVector = Icons.Outlined.CalendarToday,
+        contentDescription = stringResource(R.string.calendar),
+        tint = tint
+    )
+}
+
+@Composable
+fun RoutinesIcon(tint: Color) {
+    ImageVectorIcon(
+        imageVector = Icons.AutoMirrored.Outlined.List,
+        contentDescription = stringResource(R.string.routines),
+        tint = tint
+    )
+}
+
+@Composable
+fun ConfigurationIcon(tint: Color) {
+    ImageVectorIcon(
+        imageVector = Icons.Outlined.Settings,
+        contentDescription = stringResource(R.string.configuration),
+        tint = tint
+    )
+}
+
+@Composable
+fun ExerciseIcon(tint: Color) {
     PainterIcon(
         painter = painterResource(id = R.drawable.exercise),
-        contentDescription = stringResource(R.string.exercise)
+        contentDescription = stringResource(R.string.exercise),
+        tint = tint
     )
 }
 
@@ -147,3 +192,9 @@ data object TimedRoutineExercise : IconVariant
 data object Search : IconVariant
 
 data object Exercise : IconVariant
+
+data object Calendar : IconVariant
+
+data object Routines : IconVariant
+
+data object Configuration : IconVariant
