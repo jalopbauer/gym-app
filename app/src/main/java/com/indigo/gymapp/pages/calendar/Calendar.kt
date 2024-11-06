8c75201c0e746e3d71a0437220f670b5ceb12099
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-06 16:55:44 -0300

    Calendar Headline

--- app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
+++ app/src/main/java/com/indigo/gymapp/pages/calendar/Calendar.kt
@@ -1,9 +1,18 @@
 package com.indigo.gymapp.pages.calendar
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.indigo.gymapp.R
+import com.indigo.gymapp.common.text.Large
+import com.indigo.gymapp.common.text.headline.Headline
 import com.indigo.gymapp.manager.bottomAppBar.BottomAppBarViewModel
+import com.indigo.gymapp.ui.spacing.Spacing.Context.Padding
 
 @Composable
 fun Calendar() {
@@ -11,5 +20,16 @@ fun Calendar() {
 
     LaunchedEffect(Unit) {
         bottomAppBarViewModel.setNavigation(1)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = Padding.screen),
+    ) {
+        Headline(
+            headline = stringResource(R.string.calendar),
+            textSize = Large
+        )
     }
 }
