c5ab8fce36d5c359efcc141de7f10a7f2ff9ea6a
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-17 21:57:51 -0300

    Remove ExerciseApi

--- app/src/main/java/com/indigo/gymapp/service/api/exercises/Exercise.kt
+++
@@ -1,6 +0,0 @@
-package com.indigo.gymapp.service.api.exercises
-
-data class Exercise(
-    val id: Int,
-    val name: String
-)
--- app/src/main/java/com/indigo/gymapp/service/api/exercises/ExercisesService.kt
+++
@@ -1,7 +0,0 @@
-package com.indigo.gymapp.service.api.exercises
-
-import android.content.Context
-
-interface ExercisesService {
-    fun getExercises(context: Context, onSuccess: (List<Exercise>) -> Unit, onFail: () -> Unit, loadingFinished: () -> Unit)
-}
