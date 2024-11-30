6a747cb91abcd45b7e7a03d3989063e21205ec25
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-30 18:52:23 -0300

    Refactor RoutineCard

--- app/src/main/java/com/indigo/gymapp/routines/composable/RoutineCard.kt
+++ app/src/main/java/com/indigo/gymapp/routines/composable/RoutineCard.kt
@@ -15,7 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import com.indigo.gymapp.common.button.textButton.DeleteTextButton
 import com.indigo.gymapp.routines.entity.RoutineEntity
-import com.indigo.gymapp.ui.number.Number.Context
+import com.indigo.gymapp.ui.number.Number.Context.BorderRadius
+import com.indigo.gymapp.ui.number.Number.Context.Padding
 
 @Composable
 fun RoutineCard(
@@ -26,10 +27,10 @@ fun RoutineCard(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clip(RoundedCornerShape(Context.BorderRadius.card))
+            .clip(RoundedCornerShape(BorderRadius.card))
             .clickable { onCardClick(routine.id) }
             .background(color = MaterialTheme.colorScheme.surface)
-            .padding(PaddingValues(start=Context.Padding.card)),
+            .padding(PaddingValues(start = Padding.card)),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
     ) {
