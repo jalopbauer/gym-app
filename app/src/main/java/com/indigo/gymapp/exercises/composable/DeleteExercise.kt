bfe939a4ae610dbb8d37407b3eba2855c5658aed
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-30 16:29:48 -0300

    Create DeleteExercise.kt specify headline size

--- app/src/main/java/com/indigo/gymapp/exercises/composable/DeleteExercise.kt
+++ app/src/main/java/com/indigo/gymapp/exercises/composable/DeleteExercise.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.indigo.gymapp.R
 import com.indigo.gymapp.common.deleteButtons.DeleteButtons
+import com.indigo.gymapp.common.text.Small
 import com.indigo.gymapp.common.text.headline.Headline
 import com.indigo.gymapp.ui.number.Number.Context.Gap
 
@@ -18,7 +19,10 @@ fun DeleteExercise(cancelOnClick: () -> Unit, deleteOnClick: () -> Unit) {
             .fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(Gap.medium)
     ) {
-        Headline(headline = stringResource(R.string.deleting_this_exercise_will_delete_all_exercises_in_your_routines))
+        Headline(
+            headline = stringResource(R.string.deleting_this_exercise_will_delete_all_exercises_in_your_routines),
+            textSize = Small
+        )
         DeleteButtons(
             cancelOnClick = cancelOnClick,
             deleteOnClick = deleteOnClick
