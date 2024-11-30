956366495cbb8741f595af2b1296e56acfc1e176
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-11-30 18:46:29 -0300

    Refactor RoutinesPage to use Page Components

--- app/src/main/java/com/indigo/gymapp/routines/RoutinesPage.kt
+++ app/src/main/java/com/indigo/gymapp/routines/RoutinesPage.kt
@@ -1,26 +1,22 @@
 package com.indigo.gymapp.routines
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.indigo.gymapp.R
-import com.indigo.gymapp.common.button.floatingActionButton.FloatingActionButton
-import com.indigo.gymapp.common.icon.Add
+import com.indigo.gymapp.common.header.TextHeader
 import com.indigo.gymapp.common.navigation.NavigationIndex
+import com.indigo.gymapp.common.page.AddPage
+import com.indigo.gymapp.common.page.HeaderPage
 import com.indigo.gymapp.manager.bottomAppBar.BottomAppBarViewModel
 import com.indigo.gymapp.routines.composable.Routines
 import com.indigo.gymapp.routines.manager.RoutineViewModel
-import com.indigo.gymapp.ui.number.Number.Context.Padding
 import kotlinx.coroutines.launch
 
 @Composable
@@ -39,42 +35,41 @@ fun RoutinesPage(
 
     val coroutineScope = rememberCoroutineScope()
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Routines(
-            routines = routines,
-            onCardClick = {
-                coroutineScope.launch {
-                    routineViewModel.setRoutineId(it)
-                    onNavigateToCreateRoutine()
-                }
-            },
-            onDeleteClick = {
-//                TODO Open Bottom Sheet to confirm
-                coroutineScope.launch {
-                    routineViewModel.deleteRoutine(it)
-                }
-                Toast.makeText(
-                    context,
-                    context.getString(R.string.routine_deleted),
-                    Toast.LENGTH_SHORT
-                ).show()
+    AddPage (
+        onClick = {
+            coroutineScope.launch {
+                routineViewModel.setInitialRoutine()
             }
-        )
-
-        FloatingActionButton(
-            iconVariant = Add,
-            onClick = {
-                coroutineScope.launch {
-                    routineViewModel.setInitialRoutine()
+            onNavigateToCreateRoutine()
+        }
+    ) {
+        HeaderPage(
+            header = {
+                TextHeader(
+                    text = stringResource(id = R.string.routines)
+                )
+            }
+        ) {
+            Routines(
+                routines = routines,
+                onCardClick = {
+                    coroutineScope.launch {
+                        routineViewModel.setRoutineId(it)
+                        onNavigateToCreateRoutine()
+                    }
+                },
+                onDeleteClick = {
+//                TODO Open Bottom Sheet to confirm
+                    coroutineScope.launch {
+                        routineViewModel.deleteRoutine(it)
+                    }
+                    Toast.makeText(
+                        context,
+                        context.getString(R.string.routine_deleted),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-                onNavigateToCreateRoutine()
-            },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(Padding.button)
-        )
+            )
+        }
     }
 }
--- app/src/main/java/com/indigo/gymapp/routines/composable/RoutinesComposable.kt
+++ app/src/main/java/com/indigo/gymapp/routines/composable/RoutinesComposable.kt
@@ -1,20 +1,13 @@
 package com.indigo.gymapp.routines.composable
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.indigo.gymapp.R
-import com.indigo.gymapp.common.text.Large
-import com.indigo.gymapp.common.text.headline.Headline
 import com.indigo.gymapp.routines.entity.RoutineEntity
 import com.indigo.gymapp.ui.number.Number.Context
-import com.indigo.gymapp.ui.number.Number.Context.Padding
 
 @Composable
 fun Routines(
@@ -22,27 +15,16 @@ fun Routines(
     onCardClick: (Long) -> Unit,
     onDeleteClick: (Long) -> Unit,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = Padding.screen),
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(space = Context.Gap.default)
     ) {
-        Headline(
-            headline = stringResource(id = R.string.routines),
-            textSize = Large
-        )
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(space = Context.Gap.default)
-        ) {
-            items(routines) { routine ->
-                RoutineCard(
-                    routine = routine,
-                    onCardClick = onCardClick,
-                    onDeleteClick = onDeleteClick
-                )
-            }
+        items(routines) { routine ->
+            RoutineCard(
+                routine = routine,
+                onCardClick = onCardClick,
+                onDeleteClick = onDeleteClick
+            )
         }
     }
 }
