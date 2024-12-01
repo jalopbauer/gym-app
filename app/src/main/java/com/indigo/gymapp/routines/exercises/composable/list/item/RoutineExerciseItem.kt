9e37039e4442adc14352c1716054f7301f7499b8
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-12-01 16:44:26 -0300

    Refactor RoutineExerciseItem

--- app/src/main/java/com/indigo/gymapp/routines/exercises/composable/list/item/RoutineExerciseItem.kt
+++ app/src/main/java/com/indigo/gymapp/routines/exercises/composable/list/item/RoutineExerciseItem.kt
@@ -7,44 +7,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.indigo.gymapp.common.preview.hug.HugPreview
 import com.indigo.gymapp.common.button.radioButton.RadioButton
+import com.indigo.gymapp.common.preview.hug.HugPreview
 import com.indigo.gymapp.time.Rest
 import com.indigo.gymapp.time.Duration
 import com.indigo.gymapp.exercises.entity.ExerciseEntity
 import com.indigo.gymapp.routines.exercises.RoutineExercise
 import com.indigo.gymapp.routines.exercises.SetExercise
 import com.indigo.gymapp.routines.exercises.TimedExercise
-import com.indigo.gymapp.ui.number.Number
+import com.indigo.gymapp.routines.exercises.composable.list.item.description.RoutineExerciseItemContent
+import com.indigo.gymapp.ui.number.Number.Context.Gap
 
 @Composable
-fun RoutineExerciseListItem(
+fun RoutineExerciseItem(
     routineExercise: RoutineExercise,
     selectedRoutineExerciseId: Long?,
     selectOnClick: (Long) -> Unit
 ) {
     Row (
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.spacedBy(Gap.default),
         verticalAlignment = Alignment.CenterVertically
     ){
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(Number.Context.Gap.default),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            RadioButton(
-                selected = routineExercise.id == selectedRoutineExerciseId,
-                onClick = {
-                    selectOnClick(routineExercise.id)
-                }
-            )
-            RoutineExerciseDescription(routineExercise)
-        }
-//        Icon(
-//            imageVector = Icons.Outlined.Menu,
-//            contentDescription = stringResource(R.string.sort_item),
-//            tint = Color.Context.Icon.primary
-//        )
+        RadioButton(
+            selected = routineExercise.id == selectedRoutineExerciseId,
+            onClick = {
+                selectOnClick(routineExercise.id)
+            }
+        )
+        RoutineExerciseItemContent(
+            routineExercise = routineExercise
+        )
     }
 
 }
@@ -53,7 +46,7 @@ fun RoutineExerciseListItem(
 @Composable
 private fun SetExerciseListItemPreview() {
     HugPreview {
-        RoutineExerciseListItem(
+        RoutineExerciseItem(
             routineExercise = SetExercise(
                 exercise = ExerciseEntity(name = "Chest press"),
                 amountOfSets = 4,
@@ -69,7 +62,7 @@ private fun SetExerciseListItemPreview() {
 @Composable
 private fun TimedExerciseListItemPreview() {
     HugPreview {
-        RoutineExerciseListItem(
+        RoutineExerciseItem(
             routineExercise = TimedExercise(
                 exercise = ExerciseEntity(name = "Running"),
                 duration = Duration(30, 0)
