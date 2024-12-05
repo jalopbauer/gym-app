d1dcfe4f23b3c9ea4d7539a36dfcfc9ebce65ed7
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-12-05 16:45:12 -0300

    Duration.displayDuration

--- app/src/main/java/com/indigo/gymapp/time/Time.kt
+++ app/src/main/java/com/indigo/gymapp/time/Time.kt
@@ -4,6 +4,9 @@ import androidx.annotation.IntRange
 
 fun displaySeconds(seconds: Int) : String = if (seconds in 0 .. 9) "0$seconds" else "$seconds"
 
+fun Duration.displayDuration() : String =
+    "${this.hours}:${displaySeconds(this.minutes)}"
+
 data class Time(
     val hours: Int = 0,
     @IntRange(from = 0, to = 59)
