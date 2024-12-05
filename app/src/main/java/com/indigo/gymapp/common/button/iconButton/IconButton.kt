09f51e3ed6be6f651d6a5499fa145d290ab6d378
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-12-05 16:44:57 -0300

    Customizable tint to IconButton

--- app/src/main/java/com/indigo/gymapp/common/button/iconButton/IconButton.kt
+++ app/src/main/java/com/indigo/gymapp/common/button/iconButton/IconButton.kt
@@ -1,7 +1,9 @@
 package com.indigo.gymapp.common.button.iconButton
 
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.indigo.gymapp.common.icon.Cancel
 import com.indigo.gymapp.common.icon.Delete
 import com.indigo.gymapp.common.icon.Edit
@@ -12,10 +14,14 @@ import com.indigo.gymapp.common.icon.Save
 @Composable
 fun IconButton(
     iconVariant : IconVariant,
+    tint: Color = MaterialTheme.colorScheme.primary,
     onClick: () -> Unit
 ) {
     IconButton(onClick = onClick) {
-        Icon(iconVariant)
+        Icon(
+            iconVariant = iconVariant,
+            tint = tint
+        )
     }
 }
 
