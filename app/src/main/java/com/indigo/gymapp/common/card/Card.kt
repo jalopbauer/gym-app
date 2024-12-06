8923a383e18ab7415d2514d7c67380db6a004055
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-12-06 15:53:38 -0300

    Changed from Row to Column in Card

--- app/src/main/java/com/indigo/gymapp/common/card/Card.kt
+++ app/src/main/java/com/indigo/gymapp/common/card/Card.kt
@@ -3,14 +3,13 @@ package com.indigo.gymapp.common.card
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import com.indigo.gymapp.ui.number.Number.Context.BorderRadius
@@ -18,15 +17,14 @@ import com.indigo.gymapp.ui.number.Number.Context.Padding
 
 @Composable
 fun Card(onClick: () -> Unit, content: @Composable () -> Unit) {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .clip(RoundedCornerShape(BorderRadius.card))
             .clickable { onClick() }
             .background(color = MaterialTheme.colorScheme.surface)
             .padding(PaddingValues(start = Padding.card)),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         content()
     }
